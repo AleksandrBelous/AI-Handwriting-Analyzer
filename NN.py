e562b535948285a01a8cae5830be74322c1b01c5
AleksandrBelous <ab21082001@mail.ru>
2024-05-21 15:34:21 +0300

    -

--- NN.py
+++ NN.py
@@ -1,42 +1,45 @@
-#!/home/nemo/anaconda3/envs/keylog_tf/bin/python
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras import layers, models, callbacks, optimizers, losses, metrics
+from sklearn.model_selection import train_test_split
 import numpy as np
 
+# Создание модели
+modls = []
+a, b, c = 8, 12, 4
+for i in range(a, b, c):
+    model = models.Sequential([
+        layers.Dense(i, input_shape=(3,), activation='relu'),
+        layers.Dropout(0.5),
+        # layers.Dense(16, activation='relu'),
+        layers.Dense(1, activation='sigmoid')
+    ])
 
-class NN:
-    def __init__(self):
-        self.train_labels = None
-        self.train_sequences = None
-        self.model = models.Sequential(
-                [
-                        # Маскирование нулевых значений во входных данных
-                        layers.Masking(mask_value=0.0, input_shape=(None, 1)),
-                        # LSTM слой с 64 нейронами
-                        layers.LSTM(64),
-                        # Выходной слой с сигмоидной активацией для двоичной классификации
-                        layers.Dense(1, activation='sigmoid')
-                        ]
-                )
+    # Компиляция модели
+    model.compile(optimizer=optimizers.SGD(),
+                  loss=losses.binary_crossentropy,
+                  metrics=[metrics.binary_accuracy])
 
-    def compile_NN(self):
-        self.model.compile(optimizer='adam',
-                           loss='binary_crossentropy',
-                           metrics=['accuracy']
-                           )
+    early_stop = callbacks.EarlyStopping(monitor='val_accuracy',
+                                         patience=5,
+                                         restore_best_weights=True,
+                                         mode='max',
+                                         verbose=1)
+    checkpoints = callbacks.ModelCheckpoint(f'best_model_{i}.h5',
+                                            monitor='val_accuracy',
+                                            save_best_only=True,
+                                            mode='max',
+                                            verbose=1)
+    # Обучение модели
+    model.fit(X_train, y_train,
+              epochs=5,
+              batch_size=32,
+              validation_split=0.3,
+              callbacks=[early_stop, checkpoints],
+              verbose=True)
 
-    def get_random_datasets(self):
-        self.train_sequences = [np.random.rand(np.random.randint(1, 10), 1) for _ in range(100)]
-        self.train_labels = np.random.randint(2, size=100)
-
-    def teach_NN(self):
-        self.model.fit(self.train_sequences, self.train_labels, epochs=10, batch_size=32)
-
-    def get_prediction(self, sequence):
-        # new_sequence = np.random.rand(np.random.randint(1, 10), 1)
-        prediction = self.model.predict(np.array([sequence]))
-        print("Similarity:", prediction[0][0])
-
-
-if __name__ == '__main__':
-    print('OK')
+    # Оценка модели
+    loss, accuracy = model.evaluate(X_test, y_test, verbose=True)
+    # print(f"i = {i}: Test Accuracy: {accuracy:.4f}")
+    modls.append((accuracy, model))
+for i in range(len(modls)):
+    print(i, modls[i][0])
